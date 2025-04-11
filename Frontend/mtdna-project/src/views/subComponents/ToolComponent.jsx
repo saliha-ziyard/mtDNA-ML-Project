f125795fbc38222696814f9fdaa175ea6a524ef9
saliha-ziyard <salihaziyard@gmail.com>
2025-04-12 01:46:30 +0530

    tool compoenet  css seperation

--- Frontend/mtdna-project/src/views/subComponents/ToolComponent.jsx
+++ Frontend/mtdna-project/src/views/subComponents/ToolComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../../cssStyles/toolComponentCss.scss'
 import axios from "axios";
 import { FaSpinner, FaExclamationCircle, FaHistory } from "react-icons/fa";
 import { jsPDF } from "jspdf";
